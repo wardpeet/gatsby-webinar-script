8087f82c0331e9a4d28fbdffdfb739e14450ae38
Ward Peeters <ward@coding-tech.com>
2022-07-12 10:34:31 +0200

    send pageview

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,24 @@ import "@fontsource/dm-sans/500.css"
 import "@fontsource/dm-sans/700.css"
 import "@fontsource/dm-mono"
 import "@fontsource/dm-mono/500.css"
+
+export const onRouteUpdate = ({ location }, pluginOptions = {}) => {
+  // wrap inside a timeout to make sure react-helmet is done with its changes (https://github.com/gatsbyjs/gatsby/issues/11592)
+  const sendPageView = () => {
+    const pagePath = location
+      ? location.pathname + location.search + location.hash
+      : undefined
+    window.gtag(`event`, `page_view`, { page_path: pagePath })
+  }
+
+  if (`requestAnimationFrame` in window) {
+    requestAnimationFrame(() => {
+      requestAnimationFrame(sendPageView)
+    })
+  } else {
+    // simulate 2 rAF calls
+    setTimeout(sendPageView, 32)
+  }
+
+  return null
+}
